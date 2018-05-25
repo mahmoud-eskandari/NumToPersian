33bbd287be27edff68a53dec20baf705133f94b2
Mahmoud <sbs8@yahoo.com>
2018-05-25 06:33:21 +0430

    ورژن 2 تغییر سراسری کدها

--- num2persian.js
+++ num2persian.js
@@ -1,135 +1,151 @@
 /**
- Name:Javascript Number To Persian Convertor.
- Author:Mahmoud Eskanadri.
- Copyright:2015 http://Webafrooz.com.
- Licence: GNU Licence 2.4
- version:1.1.1
- Email:info@webafrooz.com,sbs8@yahoo.com
- Programming With love in Webafrooz.
+ * Name:Javascript Number To Persian Convertor.
+ * Author:Mahmoud Eskanadri.
+ * Copyright:2018 http://Webafrooz.com.
+ * Licence: GNU Licence 2.4
+ * version:2.0.0
+ * Email:info@webafrooz.com,sbs8@yahoo.com
+ * coded with ♥ in Webafrooz.
+ * big numbers refrence: https://fa.wikipedia.org/wiki/%D9%86%D8%A7%D9%85_%D8%A7%D8%B9%D8%AF%D8%A7%D8%AF_%D8%A8%D8%B2%D8%B1%DA%AF
  **/
+
 /**
- *
- * @type {string}
- */
-var andd = " و ";
-/**
- *
- * @type {string}
- */
-var zero = "صفر";
-/**
- *
- * @type {*[]}
- */
-var persian = [
-    ["", "یك", "دو", "سه", "چهار", "پنج", "شش", "هفت", "هشت", "نه"],
-    ["", "ده", "بیست", "سی", "چهل", "پنجاه", "شصت", "هفتاد", "هشتاد", "نود"],
-    ["", "یكصد", "دویست", "سیصد", "چهارصد", "پانصد", "ششصد", "هفتصد", "هشتصد", "نهصد"],
-    ['', '', " هزار ", " میلیون ", " میلیارد ", " تریلیون "],
-    ["ده", "یازده", "دوازده", "سیزده", "چهارده", "پانزده", "شانزده", "هفده", "هجده", "نوزده"]
-];
-/**
- *
- * @param num
- * @param tt
+ * A function for converting numbers to persian letters
  * @returns {*}
  * @constructor
  */
-function NumToPersian(num, tt) {
-    if (typeof(tt) === 'undefined') {
-        tt = 0;
+NumToPersian = (function () {
+    /**
+     *
+     * @type {string}
+     */
+    spliter = " و ";
+
+    /**
+     *
+     * @type {string}
+     */
+    zero = "صفر";
+
+    /**
+     *
+     * @type {*[]}
+     */
+    Letters = [
+        ["", "یك", "دو", "سه", "چهار", "پنج", "شش", "هفت", "هشت", "نه"],
+        ["ده", "یازده", "دوازده", "سیزده", "چهارده", "پانزده", "شانزده", "هفده", "هجده", "نوزده","بیست"],
+        ["","","بیست", "سی", "چهل", "پنجاه", "شصت", "هفتاد", "هشتاد", "نود"],
+        ["", "یكصد", "دویست", "سیصد", "چهارصد", "پانصد", "ششصد", "هفتصد", "هشتصد", "نهصد"],
+        ['', " هزار ", " میلیون ", " میلیارد ", " بیلیون ", " بیلیارد ", " تریلیون ", " تریلیارد ",
+         " کوآدریلیون ", " کادریلیارد ", " کوینتیلیون ", " کوانتینیارد ", " سکستیلیون ", " سکستیلیارد ", " سپتیلیون ",
+          " سپتیلیارد ", " اکتیلیون ", " اکتیلیارد ", " نانیلیون ", " نانیلیارد ", " دسیلیون ", " دسیلیارد ", " گوگول "]
+    ];
+
+    /**
+     * Clear number and split to 3th sections
+     * @param {*} num 
+     */
+    function PrepareNumber(num){
+        if(typeof num === "number"){
+            num = num.toString();
+        }
+        NumberLength = num.length%3;
+        if(NumberLength === 1){
+            num = "00"+num;
+        }else if(NumberLength === 2){
+            num = "0"+num;
+        }
+        //Explode to array
+        num = num.split("");
+        index = 0;
+        output= [];
+        for(i = 0;i<num.length;i++){
+            if(i >0 && i%3 === 0){
+                index++;
+            }
+            if(typeof output[index] !== "undefined"){
+                output[index]+= num[i];
+            }else{
+                output[index] = num[i];
+            }
+        }
+        return output;
     }
-    if(typeof(num) === 'number'){
-		num = num.toString();
-	}
-    var let_stack = [];
-    //num = num.toString();
-    len = num.length;
-    var chars = [];
-    if (len > 3) {
-        /** Split To 3 chars **/
-        var carry = len % 3;
-        var jc = 0;
-        if (carry > 0) {
-            chars.push(num.substring(0, carry));
+
+    /**
+     * Convert 3 numbers into letter
+     * @param {*} num 
+     */
+    function ThreeNumbersToLetter(num){
+        //return zero
+        if(parseInt(num) === 0){
+            return "";
         }
-        for (var j = carry; len > j; j++) {
-            if (jc % 3 == 0)
-                chars.push(num.substring(j, j + 3));
-            jc++;
+        parsedInt = parseInt(num);
+        if(parsedInt < 10){
+            return Letters[0][parsedInt];
         }
-        /** End Of Split **/
-        var len = chars.length;
-        var t = [];
-        for (i = 0; len > i; i++) {
-
-            var nn = NumToPersian(chars[i], 2);
-
-            if (Object.prototype.toString.call(nn) === '[object Array]') {
-                k = nn.join(andd);
-                if (persian[3][len - i] != '')
-                    k += persian[3][len - i];
-
-                t.push(k);
-            }
-
+        if(parsedInt <= 20){
+            return Letters[1][parsedInt-10];
         }
-        return (t.join(andd));
-    } else {
-        var ar = num.split('');
-        if (parseInt(num) == 0) {
-            if (tt == 0) {
-                return zero;
-            } else {
-                return '';
-            }
+        if(parsedInt < 100){
+             one = parsedInt%10;
+             ten = (parsedInt-one)/10;
+             if(one > 0){
+                return Letters[2][ten] + spliter + Letters[0][one];
+             }
+             return Letters[2][ten];
         }
-
-        if (len == 3) {
-            //FOR Min of 1000
-            if (parseInt(ar[0] + ar[1]) == 0) {
-                let_stack.push(persian[0][ar[2]]);
-            } else {
-                for (var i = 0; i < ar.length; i++) {
-                    if (ar[i] != 0) {
-                        // Sadgan
-                        if (i == 0)
-                            let_stack.push(persian[2][ar[i]]);
-                        //Dahgan
-                        if (i == 1) {
-                            var baghi = parseInt(ar[i] + ar[i + 1]);
-                            //console.log(baghi);
-                            if (baghi > 19)
-                                let_stack.push(persian[1][ar[i]]);
-                            else
-                                let_stack.push(persian[4][ar[i + 1]]);
-                        }
-                        //Yekan
-                        if (i == 2 && ar[i] != 0) {
-                            let_stack.push(persian[0][ar[i]]);
-                        }
-                    }
+        one = parsedInt%10;
+        hundreds = (parsedInt-parsedInt%100)/100;
+        ten = (parsedInt-((hundreds*100)+one))/10;
+        out = [Letters[3][hundreds]];
+        SecendPart = ((ten*10)+one);
+        if(SecendPart > 0){
+            if(SecendPart < 10){
+                out.push(Letters[0][SecendPart]);
+            }else if(SecendPart <= 20){
+                out.push(Letters[1][SecendPart-10]);
+            }else{
+                out.push(Letters[2][ten]);
+                if(one > 0){
+                    out.push(Letters[0][one]);
                 }
             }
-        } else if (len == 2) {
-            if (ar[0] != 0 && ar[1] == 0) {
-                let_stack.push(persian[1][ar[0]]);
-            } else if (ar[1] != 0 && ar[0] != 0 && parseInt(ar[0] + ar[1]) > 20) {
-                let_stack.push(persian[1][ar[0]]);
-                let_stack.push(persian[0][ar[1]]);
-            } else if (parseInt(ar[0] + ar[1]) < 20) {
-                let_stack.push(persian[4][ar[1]]);
-            } else {
-                return '';
-            }
-        } else if (len == 1) {
-            let_stack.push(persian[0][ar[0]]);
         }
-        if (tt === 0)
-            return let_stack.join(andd);
+        return out.join(spliter);
     }
-    return let_stack;
-}
+
+    /**
+     * Main function
+     */
+    return function(num){
+        //return zero
+        if(parseInt(num) === 0){
+            return zero;
+        }
+        if(num.length > 69){
+            return "خارج از محدوده";
+        }
+        //Split to sections
+        SplitedNumber = PrepareNumber(num);
+
+        //Fetch Sections and convert
+        funcout = [];
+        SplitLength = SplitedNumber.length;
+        for(i=0;i < SplitLength;i++){
+            SectionTitle = Letters[4][SplitLength-(i+1)];
+            converted = ThreeNumbersToLetter(SplitedNumber[i]);
+            if(converted !== ""){
+                funcout.push(converted+SectionTitle);
+            }
+        }
+        return funcout.join(spliter);
+    };
+})();
 String.prototype.toPersian = function () {
-    return NumToPersian(this,0);
+    return NumToPersian(this);
+};
+Number.prototype.toPersian = function () {
+    return NumToPersian(parseInt(this).toString());
 };
