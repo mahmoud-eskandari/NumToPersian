d993f74e4097db537afcd3cea7f8f08df50e2807
mahmoud <info@webafrooz.com>
2019-06-07 17:49:07 +0430

    Tag: 3.1.1 Code review

--- dist/num2persian.js
+++ dist/num2persian.js
@@ -4,7 +4,7 @@
  * Generated on 2019-06-07
  * Author:Mahmoud Eskanadri.
  * Copyright:2018 http://Webafrooz.com.
- * version:3.0.3
+ * version:3.1.1
  * Email:info@webafrooz.com,sbs8@yahoo.com
  * coded with ♥ in Webafrooz.
  * big numbers refrence: https://fa.wikipedia.org/wiki/%D9%86%D8%A7%D9%85_%D8%A7%D8%B9%D8%AF%D8%A7%D8%AF_%D8%A8%D8%B2%D8%B1%DA%AF
@@ -16,31 +16,31 @@
  *
  * @type {string}
  */
-var Delimiter = ' و ';
+var delimiter = ' و ';
 /**
  *
  * @type {string}
  */
 
-var Zero = 'صفر';
+var zero = 'صفر';
 /**
  *
  * @type {*[]}
  */
 
-var Letters = [['', 'یک', 'دو', 'سه', 'چهار', 'پنج', 'شش', 'هفت', 'هشت', 'نه'], ['ده', 'یازده', 'دوازده', 'سیزده', 'چهارده', 'پانزده', 'شانزده', 'هفده', 'هجده', 'نوزده', 'بیست'], ['', '', 'بیست', 'سی', 'چهل', 'پنجاه', 'شصت', 'هفتاد', 'هشتاد', 'نود'], ['', 'یکصد', 'دویست', 'سیصد', 'چهارصد', 'پانصد', 'ششصد', 'هفتصد', 'هشتصد', 'نهصد'], ['', ' هزار', ' میلیون', ' میلیارد', ' بیلیون', ' بیلیارد', ' تریلیون', ' تریلیارد', 'کوآدریلیون', ' کادریلیارد', ' کوینتیلیون', ' کوانتینیارد', ' سکستیلیون', ' سکستیلیارد', ' سپتیلیون', 'سپتیلیارد', ' اکتیلیون', ' اکتیلیارد', ' نانیلیون', ' نانیلیارد', ' دسیلیون', ' دسیلیارد']];
+var letters = [['', 'یک', 'دو', 'سه', 'چهار', 'پنج', 'شش', 'هفت', 'هشت', 'نه'], ['ده', 'یازده', 'دوازده', 'سیزده', 'چهارده', 'پانزده', 'شانزده', 'هفده', 'هجده', 'نوزده', 'بیست'], ['', '', 'بیست', 'سی', 'چهل', 'پنجاه', 'شصت', 'هفتاد', 'هشتاد', 'نود'], ['', 'یکصد', 'دویست', 'سیصد', 'چهارصد', 'پانصد', 'ششصد', 'هفتصد', 'هشتصد', 'نهصد'], ['', ' هزار', ' میلیون', ' میلیارد', ' بیلیون', ' بیلیارد', ' تریلیون', ' تریلیارد', 'کوآدریلیون', ' کادریلیارد', ' کوینتیلیون', ' کوانتینیارد', ' سکستیلیون', ' سکستیلیارد', ' سپتیلیون', 'سپتیلیارد', ' اکتیلیون', ' اکتیلیارد', ' نانیلیون', ' نانیلیارد', ' دسیلیون', ' دسیلیارد']];
 /**
  * Decimal suffixes for decimal part
  * @type {string[]}
  */
 
-var DecimalSuffixes = ['', 'دهم', 'صدم', 'هزارم', 'ده‌هزارم', 'صد‌هزارم', 'میلیونوم', 'ده‌میلیونوم', 'صدمیلیونوم', 'میلیاردم', 'ده‌میلیاردم', 'صد‌‌میلیاردم'];
+var decimalSuffixes = ['', 'دهم', 'صدم', 'هزارم', 'ده‌هزارم', 'صد‌هزارم', 'میلیونوم', 'ده‌میلیونوم', 'صدمیلیونوم', 'میلیاردم', 'ده‌میلیاردم', 'صد‌‌میلیاردم'];
 /**
  * Clear number and split to 3 sections
  * @param {*} num
  */
 
-var PrepareNumber = function PrepareNumber(num) {
+var prepareNumber = function prepareNumber(num) {
   var Out = num;
 
   if (typeof Out === 'number') {
@@ -59,8 +59,8 @@ var PrepareNumber = function PrepareNumber(num) {
   return Out.replace(/\d{3}(?=\d)/g, '$&*').split('*');
 };
 
-var ThreeNumbersToLetter = function ThreeNumbersToLetter(num) {
-  // return Zero
+var threeNumbersToLetter = function threeNumbersToLetter(num) {
+  // return zero
   if (parseInt(num, 0) === 0) {
     return '';
   }
@@ -68,11 +68,11 @@ var ThreeNumbersToLetter = function ThreeNumbersToLetter(num) {
   var parsedInt = parseInt(num, 0);
 
   if (parsedInt < 10) {
-    return Letters[0][parsedInt];
+    return letters[0][parsedInt];
   }
 
   if (parsedInt <= 20) {
-    return Letters[1][parsedInt - 10];
+    return letters[1][parsedInt - 10];
   }
 
   if (parsedInt < 100) {
@@ -81,117 +81,95 @@ var ThreeNumbersToLetter = function ThreeNumbersToLetter(num) {
     var _ten = (parsedInt - _one) / 10;
 
     if (_one > 0) {
-      return Letters[2][_ten] + Delimiter + Letters[0][_one];
+      return letters[2][_ten] + delimiter + letters[0][_one];
     }
 
-    return Letters[2][_ten];
+    return letters[2][_ten];
   }
 
   var one = parsedInt % 10;
   var hundreds = (parsedInt - parsedInt % 100) / 100;
   var ten = (parsedInt - (hundreds * 100 + one)) / 10;
-  var out = [Letters[3][hundreds]];
+  var out = [letters[3][hundreds]];
   var SecondPart = ten * 10 + one;
 
   if (SecondPart > 0) {
     if (SecondPart < 10) {
-      out.push(Letters[0][SecondPart]);
+      out.push(letters[0][SecondPart]);
     } else if (SecondPart <= 20) {
-      out.push(Letters[1][SecondPart - 10]);
+      out.push(letters[1][SecondPart - 10]);
     } else {
-      out.push(Letters[2][ten]);
+      out.push(letters[2][ten]);
 
       if (one > 0) {
-        out.push(Letters[0][one]);
+        out.push(letters[0][one]);
       }
     }
   }
 
-  return out.join(Delimiter);
+  return out.join(delimiter);
 };
 /**
- * Remove end zero of decimal part
+ * Convert Decimal part
+ * @param decimalPart
+ * @returns {string}
+ * @constructor
+ */
+
+
+var convertDecimalPart = function convertDecimalPart(decimalPart) {
+  // Clear right zero
+  decimalPart = decimalPart.replace(/0*$/, "");
+
+  if (decimalPart === '') {
+    return '';
+  }
+
+  if (decimalPart.length > 11) {
+    decimalPart = decimalPart.substr(0, 11);
+  }
+
+  return ' ممیز ' + Num2persian(decimalPart) + ' ' + decimalSuffixes[decimalPart.length];
+};
+/**
+ * Main function
  * @param input
  * @returns {string}
  * @constructor
  */
 
 
-var ClearEndZero = function ClearEndZero(input) {
-  var ReversedInput = input.split('').reverse();
-
-  for (var i = 0; i < ReversedInput.length; i++) {
-    if (ReversedInput[i] !== '0') {
-      return ReversedInput.reverse().join('');
-    }
-
-    i--;
-    ReversedInput.shift();
-  }
-
-  return '';
-};
-/**
- * Convert Decimal part
- * @param DecimalPart
- * @returns {string}
- * @constructor
- */
-
-
-var ConvertDecimalPart = function ConvertDecimalPart(DecimalPart) {
-  // Clear right zero
-  DecimalPart = ClearEndZero(DecimalPart);
-
-  if (DecimalPart === '') {
-    return '';
-  }
-
-  if (DecimalPart.length > 11) {
-    DecimalPart = DecimalPart.substr(0, 11);
-  }
-
-  return ' ممیز ' + Num2persian(DecimalPart) + ' ' + DecimalSuffixes[DecimalPart.length];
-};
-/**
- * Main function
- * @param InputNumber
- * @returns {string}
- * @constructor
- */
-
-
-var Num2persian = function Num2persian(InputNumber) {
+var Num2persian = function Num2persian(input) {
   // Clear Non digits
-  InputNumber = InputNumber.replace(/\D+(?<![.])/g, ''); // return Zero
+  input = input.replace(/[^0-9.]/g, ''); // return zero
 
-  if (isNaN(parseFloat(InputNumber))) {
-    return Zero;
+  if (isNaN(parseFloat(input))) {
+    return zero;
   } // Declare Parts
 
 
-  var DecimalPart = '';
-  var IntegerPart = InputNumber;
-  var PointIndex = InputNumber.indexOf('.'); // Check for float numbers form string and split Int/Dec
+  var decimalPart = '';
+  var integerPart = input;
+  var pointIndex = input.indexOf('.'); // Check for float numbers form string and split Int/Dec
 
-  if (PointIndex > -1) {
-    IntegerPart = InputNumber.substring(0, PointIndex);
-    DecimalPart = InputNumber.substring(PointIndex + 1, InputNumber.length);
+  if (pointIndex > -1) {
+    integerPart = input.substring(0, pointIndex);
+    decimalPart = input.substring(pointIndex + 1, input.length);
   }
 
-  if (IntegerPart.length > 66) {
+  if (integerPart.length > 66) {
     return 'خارج از محدوده';
   } // Split to sections
 
 
-  var SpitedNumber = PrepareNumber(IntegerPart); // Fetch Sections and convert
+  var slicedNumber = prepareNumber(integerPart); // Fetch Sections and convert
 
   var Output = [];
-  var SplitLength = SpitedNumber.length;
+  var SplitLength = slicedNumber.length;
 
   for (var i = 0; i < SplitLength; i += 1) {
-    var SectionTitle = Letters[4][SplitLength - (i + 1)];
-    var converted = ThreeNumbersToLetter(SpitedNumber[i]);
+    var SectionTitle = letters[4][SplitLength - (i + 1)];
+    var converted = threeNumbersToLetter(slicedNumber[i]);
 
     if (converted !== '') {
       Output.push(converted + SectionTitle);
@@ -199,11 +177,11 @@ var Num2persian = function Num2persian(InputNumber) {
   } // Convert Decimal part
 
 
-  if (DecimalPart.length > 0) {
-    DecimalPart = ConvertDecimalPart(DecimalPart);
+  if (decimalPart.length > 0) {
+    decimalPart = convertDecimalPart(decimalPart);
   }
 
-  return Output.join(Delimiter) + DecimalPart;
+  return Output.join(delimiter) + decimalPart;
 };
 
 String.prototype.toPersianLetter = function () {
--- src/num2persian.js
+++ src/num2persian.js
@@ -2,200 +2,184 @@
  *
  * @type {string}
  */
-const Delimiter = ' و ';
+const delimiter = ' و ';
 
 /**
  *
  * @type {string}
  */
-const Zero = 'صفر';
+const zero = 'صفر';
 
 /**
  *
  * @type {*[]}
  */
-const Letters = [
-    ['', 'یک', 'دو', 'سه', 'چهار', 'پنج', 'شش', 'هفت', 'هشت', 'نه'],
-    ['ده', 'یازده', 'دوازده', 'سیزده', 'چهارده', 'پانزده', 'شانزده', 'هفده', 'هجده', 'نوزده', 'بیست'],
-    ['', '', 'بیست', 'سی', 'چهل', 'پنجاه', 'شصت', 'هفتاد', 'هشتاد', 'نود'],
-    ['', 'یکصد', 'دویست', 'سیصد', 'چهارصد', 'پانصد', 'ششصد', 'هفتصد', 'هشتصد', 'نهصد'],
-    ['', ' هزار', ' میلیون', ' میلیارد', ' بیلیون', ' بیلیارد', ' تریلیون', ' تریلیارد',
-        'کوآدریلیون', ' کادریلیارد', ' کوینتیلیون', ' کوانتینیارد', ' سکستیلیون', ' سکستیلیارد', ' سپتیلیون',
-        'سپتیلیارد', ' اکتیلیون', ' اکتیلیارد', ' نانیلیون', ' نانیلیارد', ' دسیلیون', ' دسیلیارد'],
+const letters = [
+  ['', 'یک', 'دو', 'سه', 'چهار', 'پنج', 'شش', 'هفت', 'هشت', 'نه'],
+  ['ده', 'یازده', 'دوازده', 'سیزده', 'چهارده', 'پانزده', 'شانزده', 'هفده', 'هجده', 'نوزده', 'بیست'],
+  ['', '', 'بیست', 'سی', 'چهل', 'پنجاه', 'شصت', 'هفتاد', 'هشتاد', 'نود'],
+  ['', 'یکصد', 'دویست', 'سیصد', 'چهارصد', 'پانصد', 'ششصد', 'هفتصد', 'هشتصد', 'نهصد'],
+  ['', ' هزار', ' میلیون', ' میلیارد', ' بیلیون', ' بیلیارد', ' تریلیون', ' تریلیارد',
+    'کوآدریلیون', ' کادریلیارد', ' کوینتیلیون', ' کوانتینیارد', ' سکستیلیون', ' سکستیلیارد', ' سپتیلیون',
+    'سپتیلیارد', ' اکتیلیون', ' اکتیلیارد', ' نانیلیون', ' نانیلیارد', ' دسیلیون', ' دسیلیارد'],
 ];
 
 /**
  * Decimal suffixes for decimal part
  * @type {string[]}
  */
-const DecimalSuffixes = [
-    '',
-    'دهم',
-    'صدم',
-    'هزارم',
-    'ده‌هزارم',
-    'صد‌هزارم',
-    'میلیونوم',
-    'ده‌میلیونوم',
-    'صدمیلیونوم',
-    'میلیاردم',
-    'ده‌میلیاردم',
-    'صد‌‌میلیاردم'
+const decimalSuffixes = [
+  '',
+  'دهم',
+  'صدم',
+  'هزارم',
+  'ده‌هزارم',
+  'صد‌هزارم',
+  'میلیونوم',
+  'ده‌میلیونوم',
+  'صدمیلیونوم',
+  'میلیاردم',
+  'ده‌میلیاردم',
+  'صد‌‌میلیاردم'
 ];
 
 /**
  * Clear number and split to 3 sections
  * @param {*} num
  */
-const PrepareNumber = (num) => {
-    let Out = num;
-    if (typeof Out === 'number') {
-        Out = Out.toString();
-    }
-    const NumberLength = Out.length % 3;
-    if (NumberLength === 1) {
-        Out = `00${Out}`;
-    } else if (NumberLength === 2) {
-        Out = `0${Out}`;
-    }
-    // Explode to array
-    return Out.replace(/\d{3}(?=\d)/g, '$&*')
-        .split('*');
+const prepareNumber = (num) => {
+  let Out = num;
+  if (typeof Out === 'number') {
+    Out = Out.toString();
+  }
+  const NumberLength = Out.length % 3;
+  if (NumberLength === 1) {
+    Out = `00${Out}`;
+  } else if (NumberLength === 2) {
+    Out = `0${Out}`;
+  }
+  // Explode to array
+  return Out.replace(/\d{3}(?=\d)/g, '$&*')
+    .split('*');
 };
 
-const ThreeNumbersToLetter = (num) => {
-    // return Zero
-    if (parseInt(num, 0) === 0) {
-        return '';
-    }
-    const parsedInt = parseInt(num, 0);
-    if (parsedInt < 10) {
-        return Letters[0][parsedInt];
-    }
-    if (parsedInt <= 20) {
-        return Letters[1][parsedInt - 10];
-    }
-    if (parsedInt < 100) {
-        const one = parsedInt % 10;
-        const ten = (parsedInt - one) / 10;
-        if (one > 0) {
-            return Letters[2][ten] + Delimiter + Letters[0][one];
-        }
-        return Letters[2][ten];
-    }
-    const one = parsedInt % 10;
-    const hundreds = (parsedInt - (parsedInt % 100)) / 100;
-    const ten = (parsedInt - ((hundreds * 100) + one)) / 10;
-    const out = [Letters[3][hundreds]];
-    const SecondPart = ((ten * 10) + one);
-    if (SecondPart > 0) {
-        if (SecondPart < 10) {
-            out.push(Letters[0][SecondPart]);
-        } else if (SecondPart <= 20) {
-            out.push(Letters[1][SecondPart - 10]);
-        } else {
-            out.push(Letters[2][ten]);
-            if (one > 0) {
-                out.push(Letters[0][one]);
-            }
-        }
-    }
-    return out.join(Delimiter);
-};
-
-/**
- * Remove end zero of decimal part
- * @param input
- * @returns {string}
- * @constructor
- */
-const ClearEndZero = (input) => {
-    let ReversedInput = input.split('').reverse();
-    for(let i =0;i<ReversedInput.length;i++){
-        if(ReversedInput[i] !== '0'){
-            return ReversedInput.reverse().join('');
-        }
-        i--;
-        ReversedInput.shift();
-    }
+const threeNumbersToLetter = (num) => {
+  // return zero
+  if (parseInt(num, 0) === 0) {
     return '';
+  }
+  const parsedInt = parseInt(num, 0);
+  if (parsedInt < 10) {
+    return letters[0][parsedInt];
+  }
+  if (parsedInt <= 20) {
+    return letters[1][parsedInt - 10];
+  }
+  if (parsedInt < 100) {
+    const one = parsedInt % 10;
+    const ten = (parsedInt - one) / 10;
+    if (one > 0) {
+      return letters[2][ten] + delimiter + letters[0][one];
+    }
+    return letters[2][ten];
+  }
+  const one = parsedInt % 10;
+  const hundreds = (parsedInt - (parsedInt % 100)) / 100;
+  const ten = (parsedInt - ((hundreds * 100) + one)) / 10;
+  const out = [letters[3][hundreds]];
+  const SecondPart = ((ten * 10) + one);
+  if (SecondPart > 0) {
+    if (SecondPart < 10) {
+      out.push(letters[0][SecondPart]);
+    } else if (SecondPart <= 20) {
+      out.push(letters[1][SecondPart - 10]);
+    } else {
+      out.push(letters[2][ten]);
+      if (one > 0) {
+        out.push(letters[0][one]);
+      }
+    }
+  }
+  return out.join(delimiter);
 };
+
 
 /**
  * Convert Decimal part
- * @param DecimalPart
+ * @param decimalPart
  * @returns {string}
  * @constructor
  */
-const ConvertDecimalPart = (DecimalPart) => {
-    // Clear right zero
-    DecimalPart = ClearEndZero(DecimalPart);
-    if(DecimalPart === ''){
-        return '';
-    }
-    if(DecimalPart.length > 11){
-        DecimalPart = DecimalPart.substr(0,11);
-    }
+const convertDecimalPart = (decimalPart) => {
+  // Clear right zero
+  decimalPart = decimalPart.replace(/0*$/, "");
 
-    return ' ممیز '+ Num2persian(DecimalPart)+' '+DecimalSuffixes[DecimalPart.length];
+  if (decimalPart === '') {
+    return '';
+  }
+
+  if (decimalPart.length > 11) {
+    decimalPart = decimalPart.substr(0, 11);
+  }
+  return ' ممیز ' + Num2persian(decimalPart) + ' ' + decimalSuffixes[decimalPart.length];
 };
 
 /**
  * Main function
- * @param InputNumber
+ * @param input
  * @returns {string}
  * @constructor
  */
-const Num2persian = (InputNumber) => {
-    // Clear Non digits
-    InputNumber = InputNumber.replace(/\D+(?<![.])/g,'');
+const Num2persian = (input) => {
+  // Clear Non digits
+  input = input.replace(/[^0-9.]/g, '');
 
-    // return Zero
-    if (isNaN(parseFloat(InputNumber))) {
-        return Zero;
+  // return zero
+  if (isNaN(parseFloat(input))) {
+    return zero;
+  }
+
+  // Declare Parts
+  let decimalPart = '';
+  let integerPart = input;
+  let pointIndex = input.indexOf('.');
+  // Check for float numbers form string and split Int/Dec
+  if (pointIndex > -1) {
+    integerPart = input.substring(0, pointIndex);
+    decimalPart = input.substring(pointIndex + 1, input.length);
+  }
+
+  if (integerPart.length > 66) {
+    return 'خارج از محدوده';
+  }
+  
+  // Split to sections
+  const slicedNumber = prepareNumber(integerPart);
+  // Fetch Sections and convert
+  const Output = [];
+  const SplitLength = slicedNumber.length;
+  for (let i = 0; i < SplitLength; i += 1) {
+    const SectionTitle = letters[4][SplitLength - (i + 1)];
+    const converted = threeNumbersToLetter(slicedNumber[i]);
+    if (converted !== '') {
+      Output.push(converted + SectionTitle);
     }
+  }
 
-    // Declare Parts
-    let DecimalPart = '';
-    let IntegerPart = InputNumber;
-    let PointIndex = InputNumber.indexOf('.');
+  // Convert Decimal part
+  if (decimalPart.length > 0) {
+    decimalPart = convertDecimalPart(decimalPart);
+  }
 
-    // Check for float numbers form string and split Int/Dec
-    if (PointIndex > -1) {
-        IntegerPart = InputNumber.substring(0, PointIndex);
-        DecimalPart = InputNumber.substring(PointIndex + 1, InputNumber.length);
-    }
-
-    if (IntegerPart.length > 66) {
-        return 'خارج از محدوده';
-    }
-    // Split to sections
-    const SpitedNumber = PrepareNumber(IntegerPart);
-    // Fetch Sections and convert
-    const Output = [];
-    const SplitLength = SpitedNumber.length;
-    for (let i = 0; i < SplitLength; i += 1) {
-        const SectionTitle = Letters[4][SplitLength - (i + 1)];
-        const converted = ThreeNumbersToLetter(SpitedNumber[i]);
-        if (converted !== '') {
-            Output.push(converted + SectionTitle);
-        }
-    }
-
-    // Convert Decimal part
-    if (DecimalPart.length > 0) {
-        DecimalPart = ConvertDecimalPart(DecimalPart);
-    }
-
-    return Output.join(Delimiter) + DecimalPart;
+  return Output.join(delimiter) + decimalPart;
 };
 
 String.prototype.toPersianLetter = function () {
-    return Num2persian(this);
+  return Num2persian(this);
 };
 
 Number.prototype.toPersianLetter = function () {
-    return Num2persian(parseFloat(this).toString());
+  return Num2persian(parseFloat(this).toString());
 };
 
 export default Num2persian
