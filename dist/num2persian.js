8f527eadc66605eb5d0f9025d01cadb7f4e5cdf2
Mahmoud Eskandari <info@webafrooz.com>
2020-04-13 15:28:15 +0430

    v 3.2.1

--- dist/num2persian.js
+++ dist/num2persian.js
@@ -1,10 +1,10 @@
 /**
  * Name:Javascript Number To Persian Convertor.
  * License: GPL-2.0
- * Generated on 2019-12-07
+ * Generated on 2020-04-13
  * Author:Mahmoud Eskanadri.
  * Copyright:2018 http://Webafrooz.com.
- * version:3.2.0
+ * version:3.2.1
  * Email:info@webafrooz.com,sbs8@yahoo.com
  * coded with ♥ in Webafrooz.
  * big numbers refrence: https://fa.wikipedia.org/wiki/%D9%86%D8%A7%D9%85_%D8%A7%D8%B9%D8%AF%D8%A7%D8%AF_%D8%A8%D8%B2%D8%B1%DA%AF
@@ -34,7 +34,7 @@ var negative = 'منفی ';
  * @type {*[]}
  */
 
-var letters = [['', 'یک', 'دو', 'سه', 'چهار', 'پنج', 'شش', 'هفت', 'هشت', 'نه'], ['ده', 'یازده', 'دوازده', 'سیزده', 'چهارده', 'پانزده', 'شانزده', 'هفده', 'هجده', 'نوزده', 'بیست'], ['', '', 'بیست', 'سی', 'چهل', 'پنجاه', 'شصت', 'هفتاد', 'هشتاد', 'نود'], ['', 'یکصد', 'دویست', 'سیصد', 'چهارصد', 'پانصد', 'ششصد', 'هفتصد', 'هشتصد', 'نهصد'], ['', ' هزار', ' میلیون', ' میلیارد', ' بیلیون', ' بیلیارد', ' تریلیون', ' تریلیارد', 'کوآدریلیون', ' کادریلیارد', ' کوینتیلیون', ' کوانتینیارد', ' سکستیلیون', ' سکستیلیارد', ' سپتیلیون', 'سپتیلیارد', ' اکتیلیون', ' اکتیلیارد', ' نانیلیون', ' نانیلیارد', ' دسیلیون', ' دسیلیارد']];
+var letters = [['', 'یک', 'دو', 'سه', 'چهار', 'پنج', 'شش', 'هفت', 'هشت', 'نه'], ['ده', 'یازده', 'دوازده', 'سیزده', 'چهارده', 'پانزده', 'شانزده', 'هفده', 'هجده', 'نوزده', 'بیست'], ['', '', 'بیست', 'سی', 'چهل', 'پنجاه', 'شصت', 'هفتاد', 'هشتاد', 'نود'], ['', 'یکصد', 'دویست', 'سیصد', 'چهارصد', 'پانصد', 'ششصد', 'هفتصد', 'هشتصد', 'نهصد'], ['', ' هزار', ' میلیون', ' میلیارد', ' بیلیون', ' بیلیارد', ' تریلیون', ' تریلیارد', ' کوآدریلیون', ' کادریلیارد', ' کوینتیلیون', ' کوانتینیارد', ' سکستیلیون', ' سکستیلیارد', ' سپتیلیون', ' سپتیلیارد', ' اکتیلیون', ' اکتیلیارد', ' نانیلیون', ' نانیلیارد', ' دسیلیون', ' دسیلیارد']];
 /**
  * Decimal suffixes for decimal part
  * @type {string[]}
@@ -46,7 +46,7 @@ var decimalSuffixes = ['', 'دهم', 'صدم', 'هزارم', 'ده‌هزارم'
  * @param {*} num
  */
 
-var prepareNumber = function prepareNumber (num) {
+var prepareNumber = function prepareNumber(num) {
   var Out = num;
 
   if (typeof Out === 'number') {
@@ -65,7 +65,7 @@ var prepareNumber = function prepareNumber (num) {
   return Out.replace(/\d{3}(?=\d)/g, '$&*').split('*');
 };
 
-var threeNumbersToLetter = function threeNumbersToLetter (num) {
+var threeNumbersToLetter = function threeNumbersToLetter(num) {
   // return zero
   if (parseInt(num, 0) === 0) {
     return '';
@@ -123,7 +123,7 @@ var threeNumbersToLetter = function threeNumbersToLetter (num) {
  */
 
 
-var convertDecimalPart = function convertDecimalPart (decimalPart) {
+var convertDecimalPart = function convertDecimalPart(decimalPart) {
   // Clear right zero
   decimalPart = decimalPart.replace(/0*$/, "");
 
@@ -145,7 +145,7 @@ var convertDecimalPart = function convertDecimalPart (decimalPart) {
  */
 
 
-var Num2persian = function Num2persian (input) {
+var Num2persian = function Num2persian(input) {
   // Clear Non digits
   input = input.toString().replace(/[^0-9.-]/g, '');
   var isNegative = false;
